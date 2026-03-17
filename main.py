406bde7985b59fe5e3c8844ef3d08edad1935dee
galactic10 <gustavorios1425@gmail.com>
2026-03-16 20:15:39 -0600

    Primer commit: app Python con Docker

---
+++ main.py
@@ -0,0 +1,8 @@
+print(" Ingresa tu nombre")
+nombre = input(" Nombre")
+print()
+print(" Ingresa tu edad")
+edad = input(" Edad")
+print()
+print(" Su nombre es: " + nombre + " Su edad es: " +  edad)
+print()
